1ead656fa5d3bc33e8aa171edc2d4b996c7943bf
nicolastuka <nicolascapel@gmail.com>
2020-12-28 18:56:51 -0300

    Update

--- Camera.h
+++ Camera.h
@@ -1,3 +1,14 @@
+/**
+* @file Camera.h
+* @version 0.5
+* @date 12/28/2020
+* @author Nicolas Capel
+* @brief Clase Camera para simular proyecciones de escenas con SFML 2.5. Hereda de View.
+* @code
+* //Ejemplo de uso en archivo main.cpp
+* @endcode
+*/
+
 #ifndef CAMERA_H
 #define CAMERA_H
 
@@ -6,20 +17,72 @@
 
 using namespace sf;
 
+/**
+* @brief Clase que se encarga de manipular la visualización de escena, hereda de View por ende incorpora todos sus métodos y propiedades
+*/
+
 class Camera : public View {
 private:
+	
+	/**
+	* @brief Propiedad puntero a la ventana que aplica la View
+	*/
 	RenderWindow *window;
+	
+	/**
+	* @brief Propiedad para recordar las dimensiones de la ventana
+	*/
 	Vector2f windowDimension;
+	
+	/**
+	* @brief Propiedad para recordar el tamaño del mundo o escena más allá de los límites de la ventana
+	*/
 	Vector2f worldDimension;
+	
+	/**
+	* @brief Posición a la que enfoca la cámara en la escena
+	*/
 	Vector2f cameraPosition;
+	
+	/**
+	* @brief Propiedad para recordar el zoom establecido por el usuario
+	*/
 	float zoomCamera;
+	
+	/**
+	* @brief Propiedad para recordar el tiempo de duración del efecto shake
+	*/
 	float timeShake;
+	
+	/**
+	* @brief Propiedad para recordar la fuerza de shake a establecer en el efecto
+	*/
 	int forceShake;
+	
+	/**
+	* @brief Propiedad para recordar el tiempo que debe durar el efecto flash
+	*/
 	float timeFlash;
+	
+	/**
+	* @brief Propiedad Sprite para crear el efecto de flash
+	*/
 	Sprite sShaderFlash;
+	
+	/**
+	* @brief Propiedad Texture para crear el efecto de flash
+	*/
 	Texture tShaderFlash;
+	
+	/**
+	* @brief Shader que se aplica al Sprite sShaderFlash
+	*/
 	Shader shader;
 	
+	/**
+	* @brief Calcula la posición de la cámara según la posición del objetivo.
+	* @param target Punto X e Y de la posición del elemento seguido
+	*/
 	void CalculateCameraPosition(Vector2f target) {
 		cameraPosition = target;
 		if (cameraPosition.x < windowDimension.x / 2) {
@@ -36,10 +99,16 @@ private:
 		}
 	}
 	
+	/**
+	* @brief Centra la cámara según lo obtenido en el método anterior.
+	*/
 	void MoveCamera() {
 		setCenter(cameraPosition);
 	}
 	
+	/**
+	* @brief Ejecuta el efecto Shake.
+	*/
 	void ShakeCamera() {
 		if (timeShake > 0) {
 			cameraPosition.x += rand() % forceShake;
@@ -49,6 +118,9 @@ private:
 		}
 	}
 	
+	/**
+	* @brief Ejecuta el efecto flash.
+	*/
 	void FlashCamera() {
 		if (timeFlash > 0) {
 			shader.setUniform("active", true);
@@ -66,6 +138,14 @@ private:
 	}
 	
 public:
+	/**
+	* @brief Constructor.
+	* @param w Puntero a la ventana donde se aplica la cámara
+	* @param width Ancho de la ventana
+	* @param heifht Alto de la ventana
+	* @param worldLimitWidth Ancho del mundo o escena
+	* @param worldLimitHeight Alto del mundo o escena
+	*/
 	Camera(RenderWindow *w, int width, int height, int worldLimitWidth, int worldLimitHeight) {
 		window = w;
 		zoomCamera = 1.f;
@@ -81,6 +161,11 @@ public:
 		shader.setUniform("flashColor", sf::Glsl::Vec4(1, 1, 1, 1));
 	}
 	
+	/**
+	* @brief Método que sigue a una posición en pantalla y actualización de cámara.
+	* @param target Posición de centro de cámara
+	* @param camera Dirección de memoria de la propia instancia (menor código en implementaciones)
+	*/
 	void FollowAndUpdate(Vector2f target, Camera *camera) {
 		CalculateCameraPosition(target);
 		MoveCamera();
@@ -89,10 +174,18 @@ public:
 		window->setView(*camera);
 	}
 	
+	/**
+	* @brief Método que establece la rotación de la cámara de modo acumulativo
+	* @param value Valor en flotante que establece el valor de rotación (puede ser negativo)
+	*/
 	void Rotate(float value) {
 		setRotation(getRotation() + value);
 	}
 	
+	/**
+	* @brief Método que establece el zoom de la cámara de modo acumulativo
+	* @param factor Valor en flotante que establece el valor de zoom (no puede ser negativo)
+	*/
 	void SetZoom(float factor) {
 		setSize(windowDimension);
 		zoomCamera += factor;
@@ -102,11 +195,20 @@ public:
 		zoom(zoomCamera);
 	}
 	
+	/**
+	* @brief Método que configura como ejecutar el efecto shake
+	* @param time Valor en flotante que establece la duración del mismo
+	* @param time Valor en entero que establece la fuerza del mismo
+	*/
 	void Shake(float time, int force) {
 		timeShake = time;
 		forceShake = force;
 	}
 	
+	/**
+	* @brief Método que configura como ejecutar el efecto flash
+	* @param time Valor en flotante que establece la duración del mismo
+	*/
 	void Flash(float time) {
 		timeFlash = time;
 	}
--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]){
 	t.loadFromFile("assets/textures/foto.jpg");
 	s.setTexture(t);
 	s.setPosition(0, 0);
+	//Instancia de Camera
 	Camera camera(&w, 320, 240, t.getSize().x, t.getSize().y);
 	
 	while(w.isOpen()) {
@@ -27,23 +28,30 @@ int main(int argc, char *argv[]){
 				w.close();
 		}
 		if (Mouse::isButtonPressed(Mouse::Left)) {
+			//Rotación acumulativa de la cámara sentido inverso a las agujas del
+			//reloj
 			camera.Rotate(-0.1);
 		}
 		if (Mouse::isButtonPressed(Mouse::Right)) {
+			//Rotación acumulativa de la cámara sentido de las agujas del reloj
 			camera.Rotate(0.1);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Z)) {
+			//Zoom (aleja)
 			camera.SetZoom(0.1f);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::A)) {
+			//Zoom (acerca)
 			camera.SetZoom(-0.1f);			
 		}
 		
 		if (Keyboard::isKeyPressed(Keyboard::S)) {
+			//Efecto Shake
 			camera.Shake(1.5f, 150);
 		}
 		
 		if (Keyboard::isKeyPressed(Keyboard::F)) {
+			//Efecto Flash
 			camera.Flash(.5f);
 		}
 		
@@ -60,6 +68,7 @@ int main(int argc, char *argv[]){
 			sMosca.move(0, 5);
 		}
 	
+		//Actualización de cámara para seguimiento de entidad
 		camera.FollowAndUpdate(sMosca.getPosition(), &camera);
 		
 		w.clear(Color(255,255,255,255));
